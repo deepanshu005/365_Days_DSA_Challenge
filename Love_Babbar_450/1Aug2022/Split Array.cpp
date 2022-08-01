d20271164ce4af7b439b3a0f278f337322bf418e
Deepanshu <89800372+deepanshu005@users.noreply.github.com>
2022-08-01 23:59:49 +0530

    Create Split Array.cpp

---
+++ Love_Babbar_450/1Aug2022/Split Array.cpp
@@ -0,0 +1,14 @@
+bool splitArray(int *input, int size, int start=0, int ls=0, int rs=0 ){
+    if(start==size)	// Base case
+    	return ls==rs;	// Return true if ls==rs else false
+    if(input[start]%5==0)
+        ls+=input[start];	// ls will maintain  the sum of numbers divisible by 5
+    else if(input[start]%3==0)
+        rs+=input[start];	// rs will maintain  the sum of numbers divisible by 3 but not 5
+    else{	// if number is neither divisible by 5 nor 3
+        bool lans=splitArray( input, size, start+1, ls+input[start], rs);
+        bool rans=splitArray( input, size, start+1, ls, rs+input[start]);
+        return lans||rans;	//returns the results on the basis of truth table of OR gate
+    } 
+    return splitArray( input, size, start+1, ls, rs);
+}
