0b7a59a24de6d6aa419f71fa7a8ba6ec1ae3d13d
Deepanshu <89800372+deepanshu005@users.noreply.github.com>
2022-08-02 21:31:23 +0530

    Create Length of a LL.cpp

---
+++ Love_Babbar_450/2Aug2022/Length of a LL.cpp
@@ -0,0 +1,28 @@
+// CPP DSA CN Lec 
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+int length(Node *head){
+    int count = 0;
+    Node *temp = head;
+    while( temp != NULL ){ 	// If there is no element in the LL then while loop will not be executed
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
