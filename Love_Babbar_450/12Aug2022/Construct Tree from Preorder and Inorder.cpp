3eb3cde1be894f009662488e517a491958bec336
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2022-08-12 21:24:48 +0530

    Create Construct Tree from Preorder and Inorder.cpp

---
+++ Love_Babbar_450/12Aug2022/Construct Tree from Preorder and Inorder.cpp
@@ -0,0 +1,54 @@
+/***********************************************************
+	Following is the Binary Tree Node class structure
+
+	template <typename T>
+	class BinaryTreeNode {
+    	public : 
+    	T data;
+    	BinaryTreeNode<T> *left;
+    	BinaryTreeNode<T> *right;
+
+    	BinaryTreeNode(T data) {
+        	this -> data = data;
+        	left = NULL;
+        	right = NULL;
+    	}
+	};
+
+***********************************************************/
+BinaryTreeNode<int>* buildTreeHelper(int* in, int* pre, int inS, int inE, int preS, int preE) {
+	if (inS > inE) {
+		return NULL;
+	}
+
+	int rootData = pre[preS];
+	int rootIndex = -1;
+	for (int i = inS; i <= inE; i++) {
+		if (in[i] == rootData) {
+			rootIndex = i;
+			break;
+		}
+	}
+
+	int lInS = inS;
+	int lInE = rootIndex - 1;
+	int lPreS = preS + 1;
+	int lPreE = lInE - lInS + lPreS;
+	int rPreS = lPreE + 1;
+	int rPreE = preE;
+	int rInS = rootIndex + 1;
+	int rInE = inE;
+	BinaryTreeNode<int>*  root = new BinaryTreeNode<int>(rootData);
+	root->left = buildTreeHelper(in, pre, lInS, lInE, lPreS, lPreE);
+	root->right = buildTreeHelper(in, pre, rInS, rInE, rPreS, rPreE);
+	return root;
+}
+
+BinaryTreeNode<int>* buildTree(int *preorder, int preLength, int *inorder, int inLength) {
+    return buildTreeHelper(inorder, preorder, 0, inLength - 1, 0, inLength - 1);
+}
+/*
+BinaryTreeNode<int>* buildTree(int* in, int* pre, int size) {
+	return buildTreeHelper(inorder, preorder, 0, inLength - 1, 0, inLength - 1);
+}
+*/
