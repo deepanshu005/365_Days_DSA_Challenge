6035ac3c58bf1a7020cccbb5c38127fee354370d
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2022-08-26 22:47:03 +0530

    Update Min swaps for bracket balancing.cpp

--- Love_Babbar_450/26Aug2022/Min swaps for bracket balancing.cpp
+++ Love_Babbar_450/26Aug2022/Min swaps for bracket balancing.cpp
@@ -1,13 +1,14 @@
-// CPP DSA Love Babbar 450
-int miniNumSwaps(string brackets, int n){
+// CPP DSA Love Babbar 450(GFG)
+int minimumNumberOfSwaps(string S){
         int brkt = 0, cnt = 0;
+        int n = S.size();
         for( int  i=0 ;i<n; i++ ){
-            if( S[i] ==')' )
+            if( S[i] ==']' )
                 brkt++;
-            if( S[i] =='(' )
+            if( S[i] =='[' )
                 brkt--;
-        if(brkt > 0 && S[i] == ')') 
+        if(brkt > 0 && S[i] == ']') 
             cnt += brkt;
     }
     return cnt;
-}
+    }
