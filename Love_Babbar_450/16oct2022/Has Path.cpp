2620cca46a885ba2189d6c3ba5c4aac156acdbb6
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2022-10-16 23:45:00 +0530

    Question added

--- Love_Babbar_450/16oct2022/Has Path.cpp
+++ Love_Babbar_450/16oct2022/Has Path.cpp
@@ -1,4 +1,9 @@
 // CPP DSA CN Lec 20 Love Babbar 450
+/* Given an undirected graph G(V, E) and two vertices v1 and v2 (as integers), check if there exists any path between them or not. Print true if the path exists and false otherwise.
+Note:
+1. V is the number of vertices present in graph G and vertices are numbered from 0 to V-1. 
+2. E is the number of edges present in graph G.
+ */
 #include <iostream>
 using namespace std;
 
