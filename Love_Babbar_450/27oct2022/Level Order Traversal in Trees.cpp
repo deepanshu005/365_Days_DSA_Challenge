7a47b1b67f16ae2d7e532a5be2f8f22d3ff935f6
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2022-10-27 23:31:27 +0530

    Create Level Order Traversal in Trees.cpp
    https://practice.geeksforgeeks.org/problems/level-order-traversal/1

---
+++ Love_Babbar_450/27oct2022/Level Order Traversal in Trees.cpp
@@ -0,0 +1,185 @@
+// CPP DSA Love Babbar 450
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+
+// } Driver Code Ends
+/* A binary tree Node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ 
+ 5 5 N 4 10 N 8 5 N 8 8 N 6
+
+            5
+           /
+           5
+         /   \
+        4     10
+         \     /
+          8    5
+         / \    \
+        8   8    6
+                  
+ */               
+// qu  -> -5 -5 -4 -10 8
+// vec -> 5 5 4 10 8
+
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* root){
+        if( root==NULL )
+            return {};
+        vector<int> vec;
+        queue<Node*> qu;
+        qu.push(root);
+        vec.push_back( root->data );
+        while( !qu.empty() ){
+            Node * top = qu.front();
+            qu.pop();
+            
+            if( top->left ){
+                qu.push(top->left);
+                vec.push_back( top->left->data );
+            }
+            if( top->right ){
+                qu.push(top->right);
+                vec.push_back( top->right->data );
+            }
+        
+        }
+        return vec;
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Helper function to test mirror(). Given a binary
+   search tree, print out its data elements in
+   increasing sorted order.*/
+void inOrder(struct Node* node)
+{
+  if (node == NULL)
+    return;
+
+  inOrder(node->left);
+  printf("%d ", node->data);
+
+  inOrder(node->right);
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d ",&t);
+  while (t--)
+  {
+        string s;
+		getline(cin,s);
+		Node* root = buildTree(s);
+		Solution ob;
+        vector <int> res = ob.levelOrder(root);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+  }
+  return 0;
+}
+
+
+
+// } Driver Code Ends
