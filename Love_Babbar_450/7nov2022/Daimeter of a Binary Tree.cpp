f822020b2aa4adde4ed27fad1555f930fead6445
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2022-11-07 09:45:40 +0530

    Update Daimeter of a Binary Tree.cpp

--- Love_Babbar_450/7nov2022/Daimeter of a Binary Tree.cpp
+++ Love_Babbar_450/7nov2022/Daimeter of a Binary Tree.cpp
@@ -91,22 +91,22 @@ struct Node
         left = right = NULL;
     }
 }; */
-// Approach is we will be calculating diameter at left, right and via current node for each node and return the ma of thosw
+
 class Solution {
   public:
-    
-    // Function to return the diameter of a Binary Tree.
-    int height( Node* node){
-        if( !node ) return 0;
-        return max( height(node->left), height(node->right) )+1;
-    }
-    int diameter(Node* root) {
+    int helper( Node* root, int &ans){
         if( !root )
             return 0;
-        int leftd = diameter(root->left);
-        int rightd = diameter(root->right);
-        int rootvia = height(root->left)+height(root->right)+1;
-        return max(rootvia, max(leftd, rightd ));
+        int lh = helper( root->left, ans);
+        int rh = helper( root->right, ans);
+        ans = max( ans, lh+rh );
+        return max( lh, rh )+1;
+    }
+    // Function to return the diameter of a Binary Tree.
+    int diameter(Node* root) {
+        int ans = 0;
+        helper( root, ans );
+        return ans+1;
     }
 };
 
