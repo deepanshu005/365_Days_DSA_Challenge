b6f62a260da271f4598e7ab5b46d2d32ec7f8628
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2022-11-11 22:29:05 +0530

    Create check whether BST contains Dead end.cpp
    https://practice.geeksforgeeks.org/problems/check-whether-bst-contains-dead-end/1

---
+++ Love_Babbar_450/11nov2022/check whether BST contains Dead end.cpp
@@ -0,0 +1,121 @@
+// CPP DSA Love Babbar 450
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+
+void insert(Node ** tree, int val)
+{
+    Node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = new Node(val);
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
+}
+
+
+
+
+int getCountOfNode(Node *root, int l, int h)
+{
+
+    if (!root) return 0;
+
+
+    if (root->data == h && root->data == l)
+        return 1;
+
+    if (root->data <= h && root->data >= l)
+         return 1 + getCountOfNode(root->left, l, h) +
+                    getCountOfNode(root->right, l, h);
+
+    else if (root->data < l)
+         return getCountOfNode(root->right, l, h);
+
+
+    else return getCountOfNode(root->left, l, h);
+}
+
+
+bool isDeadEnd(Node *root);
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        Node *root;
+        Node *tmp;
+    //int i;
+
+        root = NULL;
+
+        int N;
+        cin>>N;
+        for(int i=0;i<N;i++)
+        {
+            int k;
+            cin>>k;
+            insert(&root, k);
+
+        }
+
+        
+     cout<<isDeadEnd(root);
+     cout<<endl;
+    }
+}
+
+
+// } Driver Code Ends
+
+
+/*The Node structure is
+struct Node {
+int data;
+Node * right, * left;
+};*/
+// Left mein call krenge to max element can be the root node itself
+// Right mein call krenge 
+/*You are required to complete below method */
+
+// Taken from commments section
+void helper( Node* root, int min, int max, bool &flag ){
+    if( root==NULL )
+        return ;
+    if( max-min==2 )
+        flag = true;
+    helper( root->left, min, root->data, flag );
+    helper( root->right, root->data, max, flag );
+}
+bool isDeadEnd(Node *root){
+    bool flag = false;
+    helper( root, 0, INT_MAX, flag );
+    return flag;
+}
