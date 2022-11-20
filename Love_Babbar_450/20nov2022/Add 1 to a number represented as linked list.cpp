b733b756a59cf1738a3b81c0aac2e224c6c93e6a
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2022-11-20 21:13:38 +0530

    Create Add 1 to a number represented as linked list.cpp

---
+++ Love_Babbar_450/20nov2022/Add 1 to a number represented as linked list.cpp
@@ -0,0 +1,98 @@
+// CPP DSA Love Babbar 450
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/* 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution{
+    public:
+    void recur(Node* head,int &carry){
+        if(!head)
+            return;
+
+        recur(head->next,carry);
+        int t = head->data+carry;
+        if(head->next==NULL)
+            t = t+1;
+
+        head->data = t%10;
+        carry = t/10;
+    }
+    Node* addOne(Node *head) {
+        int carry = 0;
+        recur(head,carry);
+        if(carry!=0){
+            Node* nhead = new Node(carry);
+            nhead->next = head;
+            return nhead;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        
+        Node* head = new Node( s[0]-'0' );
+        Node* tail = head;
+        for(int i=1; i<s.size(); i++)
+        {
+            tail->next = new Node( s[i]-'0' );
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head); 
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
