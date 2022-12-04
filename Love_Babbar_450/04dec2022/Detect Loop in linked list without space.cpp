6a22571dee25fb5ab80a1d6845ad954dd45f5028
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2022-12-04 10:59:43 +0530

    Update Detect Loop in linked list without space.cpp
    In prev case i was spending time in doing multiplication and for that 253rd test case was failing out of 254.
    Hence i removed the calcuations and insted i just updated element of node with -1 and test cases passed

--- Love_Babbar_450/04dec2022/Detect Loop in linked list without space.cpp
+++ Love_Babbar_450/04dec2022/Detect Loop in linked list without space.cpp
@@ -1,4 +1,35 @@
 // CPP DSA Love Babbar 450
+// No space complexity
+//{ Driver Code Starts
+//Initial template code for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+
+// } Driver Code Ends
 //User function template for C++
 
 /*
@@ -23,8 +54,8 @@ class Solution
         if( !head )
             return false;
         while( head ){
-            if( (head->data)>=0 )
-                head->data = (head->data)*(-1);
+            if( (head->data) != -1)
+                head->data = -1;
             else
                 return true;
             head = head->next;
@@ -32,3 +63,41 @@ class Solution
         return false;
     }
 };
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        if(ob.detectLoop(head) )
+            cout<< "True\n";
+        else
+            cout<< "False\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
