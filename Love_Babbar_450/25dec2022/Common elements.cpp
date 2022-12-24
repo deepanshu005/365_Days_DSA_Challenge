38604772d870560ac873b6b66b0a3dca231ddee6
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2022-12-25 00:05:46 +0530

    Create Common elements.cpp
    GFG Link : 
    https://practice.geeksforgeeks.org/problems/common-elements1132/1

---
+++ Love_Babbar_450/25dec2022/Common elements.cpp
@@ -0,0 +1,66 @@
+// CPP DSA Love Babbar 450
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:    
+        vector <int> commonElements (int A[], int B[], int C[], int a, int b, int c){
+            vector<int> vec;
+            int i=0;
+            int j=0;
+            int k=0;
+            int prev = INT_MAX;
+            while( i<a and j<b and k<c ){
+                if( A[i]==B[j] and B[j]==C[k]){
+                    if( prev!=A[i] ){// tp handle duplicates without using any other data structure
+                        vec.push_back(A[i]);    
+                    }
+                    prev = A[i];
+                    i++;
+                    j++;
+                    k++;
+                }
+                else if( A[i]<=B[j] and A[i]<=C[k]){
+                    i++;
+                }
+                else if( B[j]<=A[i] and B[j]<=C[k] )
+                    j++;
+                else // mtlb C[k] sbse chota h
+                    k++;
+        }
+        return vec;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; 
+        cin >> n1 >> n2 >> n3;
+        int A[n1];
+        int B[n2];
+        int C[n3];
+        
+        for (int i = 0; i < n1; i++) cin >> A[i];
+        for (int i = 0; i < n2; i++) cin >> B[i];
+        for (int i = 0; i < n3; i++) cin >> C[i];
+        
+        Solution ob;
+        
+        vector <int> res = ob.commonElements (A, B, C, n1, n2, n3);
+        if (res.size () == 0) 
+            cout << -1;
+        for (int i = 0; i < res.size (); i++) 
+            cout << res[i] << " "; 
+        cout << endl;
+    }
+}
+// } Driver Code Ends
