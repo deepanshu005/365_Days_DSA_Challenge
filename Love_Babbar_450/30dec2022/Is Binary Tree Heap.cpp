5665ca94028470249164338afc7363c7fcccf147
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2022-12-30 18:31:30 +0530

    Create Is Binary Tree Heap.cpp
    GFG Link : 
    https://practice.geeksforgeeks.org/problems/is-binary-tree-heap/1

---
+++ Love_Babbar_450/30dec2022/Is Binary Tree Heap.cpp
@@ -0,0 +1,139 @@
+// CPP DSA Love Babbar 450
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+// User Function template for C++
+
+// Structure of node
+/*struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};*/
+// for a binary tree structure to be heap, all the levels should be completely from left tp right filled except last level 
+class Solution {
+  public:
+    bool isHeap(struct Node* tree) {
+        if( !tree or (!tree->left and !tree->right))// means ydi akeli node h to
+            return true;
+        
+        if( !tree->left and tree->right )// ydi left exist nahi krta but right krta h means this tree is not heap
+            return false;
+        
+        if (!tree->right && tree->left->left )
+            return false;
+            
+        if (tree->right && tree->right->left && !tree->left->right)
+            return false;
+
+        if (tree->left->data > tree->data)// left wale ka data bda h instead of node
+            return false;
+            
+        if (tree->right && tree->right->data > tree->data)//
+            return false;
+        
+        return isHeap( tree->left ) and isHeap( tree->right );
+        
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Solution ob;
+        Node *root = buildTree(treeString);
+        if (ob.isHeap(root))
+            cout << 1 << endl;
+        else
+            cout << 0 << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
