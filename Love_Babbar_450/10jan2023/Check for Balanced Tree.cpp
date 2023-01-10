07206d4005ec372cf4f0fcc38abd1e1e2f0b5094
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2023-01-10 23:02:28 +0530

    Create Check for Balanced Tree.cpp
    GFG Link : 
    https://practice.geeksforgeeks.org/problems/check-for-balanced-tree/1

---
+++ Love_Babbar_450/10jan2023/Check for Balanced Tree.cpp
@@ -0,0 +1,155 @@
+// CPP DSA Love Babbar 450
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/* A binary tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution{
+    public:
+    //Function to check whether a binary tree is balanced or not.
+        //Function to find the height of a binary tree.
+    int height(struct Node* node){
+        if(!node)
+            return 0;
+    // left ko bolo wo apne height calculate krke laayega 
+    // right ko bolo wo apne height calculate krke laayega 
+    // and dono ko jo maximum hoga usme +1 kr do for root node and return 
+        int lHeight = height(node->left);
+        int rHeight = height(node->right);
+        return max( lHeight,rHeight )+1;
+    }
+    
+    bool isBalanced(Node *root){
+        if( !root )
+            return true;
+        int lefti = height(root->left);
+        int righti = height(root->right);
+        if( abs(lefti-righti)>1 )
+            return false;
+        
+        return isBalanced(root->left) and isBalanced(root->right);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+  
+
+int main() {
+
+   
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.isBalanced(root) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
