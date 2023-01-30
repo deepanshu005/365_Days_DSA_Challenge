3f239aaff1989220a79b9171b20469b6077015e3
Deepanshu <89800372+deepi-badshah@users.noreply.github.com>
2023-01-30 23:13:42 +0530

    Create Min distance between two given nodes of a Binary Tree.cpp
    GFG Link : https://practice.geeksforgeeks.org/problems/min-distance-between-two-given-nodes-of-a-binary-tree/1

---
+++ Love_Babbar_450/30jan2023/Min distance between two given nodes of a Binary Tree.cpp
@@ -0,0 +1,171 @@
+// CPP DSA Love Babbar 450
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+class Solution{
+    public:
+    /* Should return minimum distance between a and b
+    in a tree with given root*/
+    bool helper(Node*root,int n, vector<int> &v){
+        if(!root)
+            return false;
+
+        if(root->data==n){
+            v.push_back(root->data);
+            return true;
+        }
+        bool left = helper(root->left,n,v);
+        if(left){
+            v.push_back(root->data);
+            return true;
+        }
+        bool right = helper(root->right,n,v);
+        if(right){
+            v.push_back(root->data);
+            return true;
+        }
+        
+        else 
+            return false;
+    }
+
+    int findDist(Node* root, int a, int b) {
+        vector<int> v1;
+        vector<int> v2;
+
+        helper(root,a,v1);
+        helper(root,b,v2);
+        int i=v1.size()-1;
+        int j=v2.size()-1;
+        while( i>=0 and j>=0 and v1[i]==v2[j] ){
+            i--;
+            j--;
+        }
+        if( j==-1 or i==-1 ){
+            return ( max(i,j)-min(i,j) );
+        }
+        if( i==j ){
+            return i+j+2;
+        }
+        return j-i+1;
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+  
+
+int main() {
+
+   
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        
+        Node* root = buildTree(s);
+        int n1, n2;
+        scanf("%d %d ", &n1, &n2);
+        Solution ob;
+        cout << ob.findDist(root, n1, n2) << endl;
+     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
